1345ddac10ceb72601c1f0048898b2da158bed45
asift91 <asift@italentdigital.com>
2020-06-25 19:08:59 +0530

    updated script

--- sql_dump.sh
+++ sql_dump.sh
@@ -12,11 +12,11 @@ dbname=$(mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_p
 
 if [ $target_db_name = "$dbname" ]; then
   echo database is already exist.. now it will take the existing db backup and imports to target
-  mysqldump -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $target_db_name > /home/$user_name/storage/db_target_target.sql
-  mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $dbname < /home/$user_name/storage/database.sql
+  mysqldump -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $target_db_name > /home/$user_name/db_target_target.sql
+  mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $dbname < /home/$user_name/database.sql
 else
   echo database does not exist.. now it will create a new db and imports to target
   mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass -e "CREATE DATABASE $target_db_name CHARACTER SET utf8;"
-  mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $target_db_name < /home/$user_name/storage/database.sql
+  mysql -h $target_db_server_name -u $target_db_login_name -p$target_db_pass $target_db_name < /home/$user_name/database.sql
 fi
 43enuc

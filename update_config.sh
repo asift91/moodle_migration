5729d03673b2cd4f4b178957ca4cc1084385c57f
sayali0512 <63386141+sayali0512@users.noreply.github.com>
2020-06-26 18:14:16 +0530

    updated values

--- update_config.sh
+++ update_config.sh
@@ -5,11 +5,11 @@
 config_path=/home/azureadmin/storage/${5}/config.php
 
 update_target_info() {
-    sudo sed -i "s~dbhost    = 'mysql-24qd5n.mysql.database.azure.com';~dbhost    = '${1}';~" ${config_path}
+    sudo sed -i "s~dbhost    = 'mysql-vc2grz.mysql.database.azure.com';~dbhost    = '${1}';~" ${config_path}
     sudo sed -i "s~dbname    = 'moodle';~dbname    = '${4}';~" ${config_path}
-    sudo sed -i "s~dbuser    = 'dbadmin@mysql-24qd5n';~dbuser    = '${2}';~" ${config_path}
+    sudo sed -i "s~dbuser    = 'dbadmin@mysql-vc2grz';~dbuser    = '${2}';~" ${config_path}
     sudo sed -i "s~dbpass    = 'iTalent@27';~dbpass    = '${3}';~" ${config_path}
-    sudo sed -i "s~lb-24qd5n.eastus.cloudapp.azure.com~${5}~" ${config_path}
+    sudo sed -i "s~lb-vc2grz.eastus.cloudapp.azure.com~${5}~" ${config_path}
     sudo sed -i "s~dataroot  = '/azlamp/moodledata';~dataroot  = '${6}';~" ${config_path}
 }
 

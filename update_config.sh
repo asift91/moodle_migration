c6dc426ccf5dd8f53bc442caf202bab9b0347ab0
Sayali Kumbhar <asift@italentdigital.com>
2020-06-30 17:40:19 +0530

    changed hardcode values

--- update_config.sh
+++ update_config.sh
@@ -5,12 +5,12 @@
 config_path=/home/azureadmin/storage/${5}/config.php
 
 update_target_info() {
-    sudo sed -i "s~dbhost    = 'mysql-vc2grz.mysql.database.azure.com';~dbhost    = '${1}';~" ${config_path}
-    sudo sed -i "s~dbname    = 'moodle';~dbname    = '${4}';~" ${config_path}
-    sudo sed -i "s~dbuser    = 'dbadmin@mysql-vc2grz';~dbuser    = '${2}';~" ${config_path}
-    sudo sed -i "s~dbpass    = 'iTalent@27';~dbpass    = '${3}';~" ${config_path}
-    sudo sed -i "s~lb-vc2grz.eastus.cloudapp.azure.com~${5}~" ${config_path}
+    sudo sed -i "s~dbhost    = '${7}';~dbhost    = '${1}';~" ${config_path}
+    sudo sed -i "s~dbname    = '${8}';~dbname    = '${4}';~" ${config_path}
+    sudo sed -i "s~dbuser    = '${9}';~dbuser    = '${2}';~" ${config_path}
+    sudo sed -i "s~dbpass    = '${10}';~dbpass    = '${3}';~" ${config_path}
+    sudo sed -i "s~${11}~${5}~" ${config_path}
     sudo sed -i "s~dataroot  = '/azlamp/moodledata';~dataroot  = '${6}';~" ${config_path}
 }
 
-update_target_info ${1} ${2} ${3} ${4} ${5} ${6}
+update_target_info ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11}
